1ed01d5051779408329a72b6cdbb9d464084b328
Lalith Kumar <90268212+LexiLominite@users.noreply.github.com>
2023-10-31 04:53:55 +0530

    Create payloadslicer.py

---
+++ payloadslicer.py
@@ -0,0 +1,17 @@
+def main():
+    payload = input("Input your payload here: ")
+
+    # Get padding number with default as 50
+    try:
+        padding = input("Enter the padding number [50 is default]: ")
+        n = int(padding) if padding else 50
+    except ValueError:
+        print("Invalid input. Using default padding of 50.")
+        n = 50
+
+    # Print the payload in chunks
+    for i in range(0, len(payload), n):
+        print("Str = Str " + '"' + payload[i:i+n] + '"')
+
+if __name__ == "__main__":
+    main()
