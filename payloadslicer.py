5e77ef3b4e49e4b28587551831f06463d4e97971
Lalith Kumar <90268212+LexiLominite@users.noreply.github.com>
2023-10-31 07:28:16 +0530

    Update payloadslicer.py

--- payloadslicer.py
+++ payloadslicer.py
@@ -8,19 +8,17 @@ def main():
         print("Invalid input. Using default variable name 'Str'")
         variable = "Str"
         
-
     # Get padding number with default as 50
     try:
         padding = input("Enter the padding number [50 is default]: ")
         n = int(padding) if padding else 50
-        
     except ValueError:
         print("Invalid input. Using default padding of 50.")
         n = 50
 
     # Print the payload in chunks
     for i in range(0, len(payload), n):
-        print("Str = Str + " + '"' + payload[i:i+n] + '"')
+        print(f'{variable} = {variable} + "{payload[i:i+n]}"')
 
 if __name__ == "__main__":
     main()
